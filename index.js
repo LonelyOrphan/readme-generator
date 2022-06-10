b0dcbb7347c2fea3ef14d101065618eb210e2bf7
Natasha Beddow <beddow90@gmail.com>
2022-06-10 15:52:55 +0100

    updated package.json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Question 1?", "Question 2?", "Question 3?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
